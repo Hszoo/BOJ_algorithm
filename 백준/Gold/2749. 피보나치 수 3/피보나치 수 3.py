249b396a9d45ce8c054b2fd95825e182d099caa6
Seong Ju Hong <97530721+Hszoo@users.noreply.github.com>
2024-01-07 23:23:16 +0900

    [Gold II] Title: 피보나치 수 3, Time: 464 ms, Memory: 90072 KB -BaekjoonHub

--- 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.py
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.py
@@ -1,24 +1,26 @@
 import sys
 
-def iter_fibo(n, memo, period):
+def iter_fibo(n, memo, period, mod):
     if n < 2:
         return n
 
     memo[0] = 0
     memo[1] = 1
     i = 2
-    while(i < 15 * 10 ** 5) :
-        memo[i] = (memo[i-1] + memo[i-2]) % (10 ** 6)
+
+    while(i < period) :
+        memo[i] = (memo[i-1] + memo[i-2]) % (mod)
         i += 1
-    
+
     while(n >= i - 1) : 
         n %= period
     return memo[n]
 
 n = int(sys.stdin.readline())
+mod = 10 ** 6
 period = 15 * 10 ** 5
 memo = [-1] * (period + 1)
 
-result = iter_fibo(n, memo, period)
+result = iter_fibo(n, memo, period, mod)
 
 print(result)
